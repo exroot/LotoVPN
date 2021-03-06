8b98f1708affd54a22301b2d86883c8242db86be
exroot <exroot.dev@gmail.com>
2021-03-06 13:46:41 -0400

    Updating testimoni section

--- components/Testimoni.js
+++ components/Testimoni.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
-
-// import react slick
 import Slider from "react-slick";
-import Image from "next/image";
-import Stars from "../public/assets/Icon/stars.svg";
 import ArrowBack from "../public/assets/Icon/eva_arrow-back-fill.svg";
 import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
+import TestimoniCard from "./TestimoniCard";
 
-const Testimoni = ({
-  listTestimoni = [
+const Testimoni = () => {
+  const [data, _] = useState([
     {
       name: "iezh Robert",
       image: "/assets/people-3.png",
@@ -45,8 +42,7 @@ const Testimoni = ({
       testimoni:
         "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LotoVPN always the best",
     },
-  ],
-}) => {
+  ]);
   const settings = {
     dots: true,
     customPaging: function (i) {
@@ -89,36 +85,8 @@ const Testimoni = ({
         ref={setSliderRef}
         className="flex items-stretch justify-items-stretch"
       >
-        {listTestimoni.map((listTestimonis, index) => (
-          <div className="px-3 flex items-stretch" key={index}>
-            <div className="border-2 border-gray-500 hover:border-indigo-500 transition-all rounded-lg p-8 flex flex-col">
-              <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
-                <div className="flex order-2 xl:order-1">
-                  <Image
-                    src={listTestimonis.image}
-                    height={50}
-                    width={50}
-                    alt="Icon People"
-                  />
-                  <div className="flex flex-col ml-5 text-left">
-                    <p className="text-lg text-black-600 capitalize">
-                      {listTestimonis.name}
-                    </p>
-                    <p className="text-sm text-black-500 capitalize">
-                      {listTestimonis.city},{listTestimonis.country}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
-                  <p className="text-sm">{listTestimonis.rating}</p>
-                  <span className="flex ml-4">
-                    <Stars className="h-4 w-4" />
-                  </span>
-                </div>
-              </div>
-              <p className="mt-5 text-left">“{listTestimonis.testimoni}”.</p>
-            </div>
-          </div>
+        {data.map((testimoni, index) => (
+          <TestimoniCard data={testimoni} key={index} />
         ))}
       </Slider>
       <div className="flex w-full items-center justify-end">
